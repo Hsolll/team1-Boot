b1d378b38924dcab85d99813b24325ea1dadf8a7
Hsolll <doc9345@naver.com>
2023-03-20 15:16:03 +0900

    new

---
+++ src/test/java/com/omb/Team1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
