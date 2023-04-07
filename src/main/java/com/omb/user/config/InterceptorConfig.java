91817d62592cf9070acb3cac2b1d9f9426cfe672
kim_yunki <kim_yunki@localhost>
2023-04-07 13:21:01 +0900

    꼭!! 기존 SQL DB에 있던 TB_USER_PRODUCT 와 TB_USER_PRODUCT_LIKE 테이블의 레코드를 모두 지운후 커밋하고 CSV파일로 적용 후 커밋 해주세요!

--- src/main/java/com/omb/user/config/InterceptorConfig.java
+++ src/main/java/com/omb/user/config/InterceptorConfig.java
@@ -22,7 +22,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
     
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        
+        /*
         notLoadList.add("/login");
         notLoadList.add("/socialLogin");
         notLoadList.add("/callback");
