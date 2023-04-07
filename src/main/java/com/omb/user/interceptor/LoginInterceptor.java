3956581101a678823a07b0e6f8c39a4c27542552
sec <sec@LAPTOP-4UHFTT13>
2023-04-07 16:34:08 +0900

    0407 김민석 커밋

--- src/main/java/com/omb/user/interceptor/LoginInterceptor.java
+++ src/main/java/com/omb/user/interceptor/LoginInterceptor.java
@@ -26,7 +26,7 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         
-        logger.info("LoginInterceptor - {}", "호출완료");
+        //logger.info("LoginInterceptor - {}", "호출완료");
         
         HttpSession session = request.getSession();
         MemberVO mvo =(MemberVO) session.getAttribute("memberLogin");
@@ -35,6 +35,7 @@ public class LoginInterceptor implements HandlerInterceptor {
             return true;
         } else {
               try { 
+            	  logger.info("LoginInterceptor - {}", "호출완료");
             	  response.setContentType("text/html; charset=UTF-8");
               PrintWriter out = response.getWriter();
               out.println("<script>alert('로그인이 필요합니다. 로그인페이지로 이동합니다.'); location.href='/login';</script>");
